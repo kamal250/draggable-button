8499b2267a4467933b855aa7eb063f3e9f711b4b
Kamal Joshi <kamal.joshi@iflair.com>
2020-07-06 20:12:44 +0530

    Added react hook for styles

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Draggable from "react-draggable";
 
 const DraggableButton = () => {
+  const initialBackground = {
+    width: `900px`,
+    height: `500px`,
+    background: "#6ccdc5",
+  };
+  const handleOnDrag = () => {
+    //   Do some fun with height/width
+    setBackground({
+      width: `900px`,
+      height: `500px`,
+      background: "#000",
+    });
+  };
+  const [background, setBackground] = useState(initialBackground);
   return (
-    <Draggable>
-      <div>Drag this</div>
-    </Draggable>
+    <div style={background}>
+      <Draggable onDrag={() => handleOnDrag()}>
+        <div>Drag this</div>
+      </Draggable>
+    </div>
   );
 };
 
